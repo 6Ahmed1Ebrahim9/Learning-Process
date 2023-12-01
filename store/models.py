af144f268d8f7b7bef5317eb9c35ce163a0b6c9f
Ahmed Ebrahim <ahmedebrahimabbas@gmail.com>
2023-12-02 00:34:30 +0200

    Add membership and payment Choice (status) Fields to Customer and Order models

--- store/models.py
+++ store/models.py
@@ -11,8 +11,31 @@ class Product(models.Model):
     last_updated = models.DateTimeField(auto_now=True) # auto_now=True means it will update every time we save the model
     
 class Customer(models.Model):
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_BRONZE, 'Bronze'),
+        (MEMBERSHIP_SILVER, 'Silver'),
+        (MEMBERSHIP_GOLD, 'Gold'),
+    ]
+    
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True) # email field will validate that it is a valid email and unique=True means it can only be used once
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True) # null=True means it can be empty
+    member_ship = models.CharField(max_length=255, choices=MEMBERSHIP_CHOICES, default= MEMBERSHIP_BRONZE)
+    
+class Order(models.Model):
+    PAYMENT_STATUS_PENDING = 'P'
+    PAYMENT_STATUS_COMPLETE = 'C'
+    PAYMENT_STATUS_FAILED = 'F'
+    PAYMENT_STATUS_CHOICES = [
+        (PAYMENT_STATUS_PENDING, 'Pending'),
+        (PAYMENT_STATUS_COMPLETE, 'Complete'),
+        (PAYMENT_STATUS_FAILED, 'Failed'),
+    ]
+    
+    placed_at = models.DateTimeField(auto_now_add=True) # auto_now_add=True means it will only update once when we create the model
+    payment_status = models.CharField(max_length=255, choices=PAYMENT_STATUS_CHOICES, default= PAYMENT_STATUS_PENDING)
