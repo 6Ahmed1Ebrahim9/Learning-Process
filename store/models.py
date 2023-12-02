a8b4d28288bc69f6897cd7a4fafe222435a71e7a
Ahmed Ebrahim <ahmedebrahimabbas@gmail.com>
2023-12-02 20:44:26 +0200

    Defining One-to-many Relationships

--- store/models.py
+++ store/models.py
@@ -3,13 +3,18 @@ from django.db import models
 # Create your models here.
 # we dont need to create a primary key because django will do it for us automatically
 # or we can create our own primary key by adding id = models.anythingField(primary_key=True)
+
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+
+
 class Product(models.Model):
     title = models.CharField(max_length=255) # because it is limited
     description = models.TextField() # because it can be long
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_updated = models.DateTimeField(auto_now=True) # auto_now=True means it will update every time we save the model
-    
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
     MEMBERSHIP_SILVER = 'S'
@@ -27,6 +32,8 @@ class Customer(models.Model):
     birth_date = models.DateField(null=True) # null=True means it can be empty
     member_ship = models.CharField(max_length=255, choices=MEMBERSHIP_CHOICES, default= MEMBERSHIP_BRONZE)
     
+    
+    
 class Order(models.Model):
     PAYMENT_STATUS_PENDING = 'P'
     PAYMENT_STATUS_COMPLETE = 'C'
@@ -39,9 +46,25 @@ class Order(models.Model):
     
     placed_at = models.DateTimeField(auto_now_add=True) # auto_now_add=True means it will only update once when we create the model
     payment_status = models.CharField(max_length=255, choices=PAYMENT_STATUS_CHOICES, default= PAYMENT_STATUS_PENDING)
-
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT) 
+    
+    
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    
 # reverse relationship between address and customer is created automatically by django so we dont need to create it
 class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
-    customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True) # primary_key=True so that we can only have one address per customer
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE) # primary_key=True so that we can only have one address per customer
+    
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
